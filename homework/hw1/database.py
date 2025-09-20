9a3a8c7eb7330c41a84ead3eb669f770a1a1a438
maksim <aleks_320@mail.ru>
2025-09-20 20:48:32 +0300

    update database.py

--- homework/hw1/database.py
+++ homework/hw1/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-# from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base
 
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
