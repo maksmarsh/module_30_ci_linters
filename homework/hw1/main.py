dd25958c9d12ab1316d47c98b7d9758c602d6b8e
maksim <aleks_320@mail.ru>
2025-09-21 17:36:41 +0300

    update

--- homework/hw1/main.py
+++ homework/hw1/main.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import FastAPI, HTTPException
 from sqlalchemy.future import select
-import models
+from models import Descriptions, Recipes, Base
 import schemas
 from database import engine, session
 from contextlib import asynccontextmanager
@@ -10,7 +10,7 @@ from contextlib import asynccontextmanager
 @asynccontextmanager
 async def lifespan(application: FastAPI):
     async with engine.begin() as conn:
-        await conn.run_sync(models.Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
         yield
         await session.close()
         await engine.dispose()
@@ -20,35 +20,34 @@ app = FastAPI()
 
 
 @app.post('/descriptions_recipe/', response_model=schemas.DescriptionsOut)
-async def descriptions(description: schemas.DescriptionsIn) -> models.Descriptions:
-    new_description = models.Descriptions(**description.dict())
+async def descriptions(description: schemas.DescriptionsIn) -> Descriptions:
+    new_description = Descriptions(**description.dict())
     async with session.begin():
         session.add(new_description)
-    new_recipe = models.Recipes(
+    new_recipe = Recipes(
         id=new_description.id,
         dish_name=new_description.dish_name,
         cooking_time=new_description.cooking_time,
     )
     async with session.begin():
         session.add(new_recipe)
-
     return new_description
 
 
 @app.get('/recipes/', response_model=List[schemas.RecipesOut])
-async def recipes() -> List[models.Recipes]:
+async def recipes() -> List[Recipes]:
     res = await session.execute(
-        select(models.Recipes).order_by(
-            models.Recipes.number_of_views.desc(), models.Recipes.cooking_time
+        select(Recipes).order_by(
+            Recipes.number_of_views.desc(), Recipes.cooking_time
         )
     )
     return list(res.scalars().all())
 
 
 @app.get('/descriptions_recipe/{recipe_id}', response_model=schemas.DescriptionsOut)
-async def recipes_id(recipe_id) -> models.Descriptions:
+async def recipes_id(recipe_id) -> Descriptions:
     recipe = (
-        (await session.execute(select(models.Descriptions).filter_by(id=recipe_id)))
+        (await session.execute(select(Descriptions).filter_by(id=recipe_id)))
         .scalars()
         .first()
     )
